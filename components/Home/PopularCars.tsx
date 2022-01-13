e75ab3709654edbf2c14df179fbc1025c3f43390
HarryNdegwa <harrisonndegwa65@gmail.com>
2022-01-13 22:31:38 +0300

    Fixed component heights

--- components/Home/PopularCars.tsx
+++ components/Home/PopularCars.tsx
@@ -13,36 +13,30 @@ interface IProps {}
 export const PopularCars: FC<IProps> = (props) => {
   const [mainLoading, setMainLoading] = useState<boolean>(true);
   const { data, loading } = useGetPopularCarsQuery();
-  // console.log("data :>> ", data);
 
   useEffect(() => {
     if (data && !loading) {
       setMainLoading(false);
     }
   }, [data, loading]);
+
   return (
     <div className="customContainer my-5">
       <p className="text-center">Popular cars right now</p>
       <h3 className="mb-4 text-center">
         Find great deals from top-rated hosts
       </h3>
-      <div className="cars-wrapper popular-cars ">
+      <div className={`${mainLoading && `popular-cars`}`}>
         {mainLoading ? (
           <FlexibleLoader />
         ) : (
-          data?.getPopularCars.map((car, idx) => (
-            <CarBox data={car} key={idx} />
-          ))
+          <div className="cars-wrapper">
+            {data?.getPopularCars.map((car, idx) => (
+              <CarBox data={car} key={idx} />
+            ))}
+          </div>
         )}
-
-        {/* Should be available cars */}
       </div>
-      {/* <div className="mt-5 my-3 d-flex justify-content-between align-items-center">
-        <p className="m-0 font-weight-bold">{data && data.length} results</p>
-        <Link to="/cars" className="m-0">
-          <Icon icon="bi:arrow-right" className="popular-cars-arrow" />
-        </Link>
-      </div> */}
     </div>
   );
 };
