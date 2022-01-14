f633caa4a6121783e33431dfaed6286c1f42de50
HarryNdegwa <harrisonndegwa65@gmail.com>
2022-01-14 17:44:55 +0300

    Changed url for deploy

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:4000/";
-// export const baseUrl = "https://caradil-server.herokuapp.com/";
+// export const baseUrl = "http://localhost:4000/";
+export const baseUrl = "https://caradil-server.herokuapp.com/";
