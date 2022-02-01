e235b215206c236bcc94642e7a373d4abf552fa7
HarryNdegwa <harrisonndegwa65@gmail.com>
2022-02-01 13:40:11 +0300

    Fix

--- components/Root/Listings/EditRequestBox.tsx
+++ components/Root/Listings/EditRequestBox.tsx
@@ -1,8 +1,7 @@
 import React, { Dispatch, FC, SetStateAction, SyntheticEvent } from "react";
 import {
   Car,
-  useEditCarBeingEditedMutation,
-  useEditCarVerifiedMutation,
+  // useEditCarBeingEditedMutation
 } from "../../../graphql_types/generated/graphql";
 import { ButtonLoading } from "../../Loading/ButtonLoading";
 
@@ -13,18 +12,18 @@ interface EditRequestBoxProps {
 }
 
 export const EditRequestBox: FC<EditRequestBoxProps> = (props) => {
-  const [editBeingEdited, { loading }] = useEditCarBeingEditedMutation();
+  // const [editBeingEdited, { loading }] = useEditCarBeingEditedMutation();
 
   const handleClick = async (e: SyntheticEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
-      const response = await editBeingEdited({
-        variables: { carId: props.data.id! },
-      });
-      if (response.data?.editCarBeingEdited) {
-        let tempCars = props.cars.filter((car) => car.id !== props.data.id);
-        props.setCars([...tempCars]);
-      }
+      // const response = await editBeingEdited({
+      //   variables: { carId: props.data.id! },
+      // });
+      // if (response.data?.editCarBeingEdited) {
+      //   let tempCars = props.cars.filter((car) => car.id !== props.data.id);
+      //   props.setCars([...tempCars]);
+      // }
     } catch (error) {
       console.log("error :>> ", error);
     }
@@ -32,15 +31,19 @@ export const EditRequestBox: FC<EditRequestBoxProps> = (props) => {
   return (
     <div className="shadow p-3 mb- d-flex align-items-center justify-content-between">
       <h6>{props.data.name}</h6>
-      <button className="btn bgOrange" onClick={handleClick} disabled={loading}>
-        {loading ? (
+      <button
+        className="btn bgOrange"
+        onClick={handleClick}
+        // disabled={loading}
+      >
+        {/* {loading ? (
           <ButtonLoading
             spinnerColor="white"
             dimensions={{ height: "18px", width: "18px" }}
           />
         ) : (
           "Grant"
-        )}
+        )} */}
       </button>
     </div>
   );
