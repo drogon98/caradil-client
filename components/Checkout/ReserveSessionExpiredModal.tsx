b6d47aa5982247b178652ca62fb7cb9ffc978228
drogon98 <harrisonndegwa65@gmail.com>
2022-05-23 21:39:14 +0300

    Updated modal reserve duration text

--- components/Checkout/ReserveSessionExpiredModal.tsx
+++ components/Checkout/ReserveSessionExpiredModal.tsx
@@ -20,14 +20,16 @@ export default function ReserveSessionExpiredModal(props: Props): ReactElement {
           Ooops! It seems your session to reserve this car has expired and
           another user has already booked this car. Due to booking
           competitiveness this might happen when it takes you longer to book a
-          car. By default, each guest has 20 mins to book a car after which the
-          car is published back to the public.
+          car. By default, each guest has{" "}
+          {props.car.end_user_type === "self_driven" ? "30" : "20"} mins to book
+          a car after which the car is published back to the public.
         </p>
 
         <br />
 
         <p>
-          Worry not, we have other cars like this one. Try to spend the 20 mins
+          Worry not, we have other cars like this one. Try to spend the{" "}
+          {props.car.end_user_type === "self_driven" ? "30" : "20"} mins
           reservation time wisely to effect your booking successfully. Thank you
         </p>
 
